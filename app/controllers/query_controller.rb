2711c845932cb79efb62ef2049fc804e2b44a199
professorjrod <mumawjared@gmail.com>
2023-01-24 18:52:54 -0800

    Add helper for url

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,7 +1,7 @@
 class QueryController < ApplicationController
-  def create 
+  def create
     @query = Query.new(query_params)
-    if(@query.save)
+    if @query.save
       redirect_to @query
     else
       redirect_to Query.where(query: @query.query, location: @query.location)[0]
@@ -11,12 +11,16 @@ class QueryController < ApplicationController
   def show
     @query = Query.find(params[:id])
     @listings = @query.listings
-    @url = "https://linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=#{@query.query}%20&location=#{@query.location}&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0"
+    @url = generated_query_url
   end
 
   private
 
   def query_params
     params.permit(:query, :location)
+  end
+
+  def generated_query_url
+    "https://linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=#{@query.query}%20&location=#{@query.location}&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0"
   end
 end
